253ff4e86012858ea072b6f261857fa0ea5a36f2
Natalie Gokkaya <natkaya@yahoo.com>
2025-09-14 10:43:35 +0100

    Exercise 2

--- scrimba.js
+++ scrimba.js
@@ -1,3 +1,9 @@
-let ageOfApplicant = 50
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
 
-console.log(ageOfApplicant)
+
+let myAge = 50;
+let convertToDogYears = 7;
+let myDogAge = myAge * convertToDogYears;
+console.log(myDogAge);
